af7d7f45a62649738489c303ebe795546343753e
zikrya <77081809+zikrya@users.noreply.github.com>
2023-07-22 19:54:01 -0400

    Home Page Update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <section className="hero is-fullheight is-bold is-primary">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="title is-size-1">
+            Travel Expert: AI-Powered Travel Companion for Your Perfect Adventure
+          </h1>
+          <h2 className="subtitle is-size-4">
+            Unleash the Power of AI to Discover Your Dream Destination! Travel Expert is your
+            ultimate travel companion, harnessing the cutting-edge capabilities of artificial
+            intelligence to curate personalized travel recommendations just for you. Say goodbye to
+            hours of research and indecision – let Travel Expert lead you to the most breathtaking
+            destinations, tailored to your preferences and interests. Embark on unforgettable
+            journeys with confidence, as AI takes the guesswork out of travel planning and unlocks
+            the world's hidden gems for your exploration!
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Landing
+export default Landing;
