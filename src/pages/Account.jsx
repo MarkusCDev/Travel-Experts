a453c506e60b39e26b008518dda7e0048349a708
MarkusCDev <markus.cc01@gmail.com>
2023-07-24 20:03:42 -0400

    revert back

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useUserAuth } from "../components/UserAuth";
+import { db } from "../firebase";
+import { Link } from "react-router-dom";
 
 const Account = () => {
-  return <div>Account</div>;
+
+  const {user} = useUserAuth();
+
+  return <div>{user?.email}</div>;
 };
 
 export default Account;
