e414c5ca52c2dd034c1b39625bc1765bfd87ad8c
zikrya <77081809+zikrya@users.noreply.github.com>
2023-07-24 22:15:24 -0400

    Added: Map opens when ai is used

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,24 @@ import ChatGpt from "../components/ChatGpt";
 import Map from "../components/Map";
 
 const Home = () => {
-  const [position, setPosition] = useState({ lat: 40.820150578444924, lng: -73.949533933551});
+  const [position, setPosition] = useState(null);
+  const [showMap, setShowMap] = useState(false);  // New state variable to control map visibility
+
+  // Modify this handler to also change the showMap state
+  const handleLocationReceived = (loc) => {
+    setPosition(loc);
+    setShowMap(true);  // Show the map when a location is received
+  };
 
   
   return (
     <div className="container">
       <div className="columns is-centered">
         <div className="column is-half">
-          <Map position={position} />
+          {showMap && position && <Map position={position} />}
         </div>
         <div className="column is-half">
-          <ChatGpt onLocationReceived={(loc) => setPosition(loc)} />
+          <ChatGpt onLocationReceived={handleLocationReceived} />
         </div>
       </div>
     </div>
