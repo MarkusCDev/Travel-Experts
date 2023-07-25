ab58f3fdfde39fcab04f58e90db31f5c39b65f26
zikrya <77081809+zikrya@users.noreply.github.com>
2023-07-25 01:12:42 -0400

    Fix: Added more styling

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -177,20 +177,23 @@ const Blogs = () => {
                     />
                   )}
                 </div>
-                <p>{blog.msg}</p>
+                <div className="centered-text">
+                <p className="centered">{blog.msg}</p>
+                </div>
                 <div>
                   <div className="control">
-                    <button
-                      className="button is-link"
-                      onClick={() =>
-                        setCommentVisibility((prevVisibility) => ({
-                          ...prevVisibility,
-                          [blog.id]: !prevVisibility[blog.id],
-                        }))
-                      }
-                    >
-                      {commentVisibility[blog.id] ? "Hide Comments" : "Show Comments"}
-                    </button>
+                  <button
+  className="is-link no-background underline-on-hover is-primary"
+  onClick={() =>
+    setCommentVisibility((prevVisibility) => ({
+      ...prevVisibility,
+      [blog.id]: !prevVisibility[blog.id],
+    }))
+  }
+>
+  {commentVisibility[blog.id] ? "Hide" : "Comment"}
+</button>
+
                   </div>
                 </div>
 
@@ -204,7 +207,7 @@ const Blogs = () => {
                   >
                     <div className="field has-addons">
                       <div className="control is-expanded">
-                       
+
                         <input
                           className="input"
                           type="text"
